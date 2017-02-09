d1d3985207f31287645507f2f0fb7a57e7b76862
Gabe Blair <gabe@castironcoding.com>
2017-02-09 10:46:18 -0800

    [F] Expose cache clear methods to PHP environment
    This makes it possible to crush the cache from other applications, for
    example when running tests, etc.

--- Classes/ClearCache.php
+++ Classes/ClearCache.php
@@ -1,7 +1,7 @@
 <?php
 namespace CIC\Cleartypo3cache;
 
-use TYPO3\CMS\Core\Utility\CommandUtility;
+use CIC\Cleartypo3cache\Service\CacheDestroyer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 if (!defined ('TYPO3_cliMode')) die ('Access denied: CLI only.');
@@ -13,10 +13,17 @@ if (!defined ('TYPO3_cliMode')) die ('Access denied: CLI only.');
  */
 class ClearCache extends \TYPO3\CMS\Core\Controller\CommandLineController {
     /**
+     * @var CacheDestroyer
+     */
+    protected $cacheDestroyer;
+
+    /**
      * constructor
      */
     public function __construct() {
         parent::__construct();
+
+        $this->cacheDestroyer = GeneralUtility::makeInstance('CIC\Cleartypo3cache\Service\CacheDestroyer');
         $this->cli_options = array_merge($this->cli_options, array());
         $this->cli_help = array_merge($this->cli_help, array(
             'name' => 'tx_cleartypo3cache_cli_cli',
@@ -40,119 +47,20 @@ class ClearCache extends \TYPO3\CMS\Core\Controller\CommandLineController {
             switch ($this->getAction()) {
                 case 'all':
                 case 'pages':
-                    $this->clearTypo3Cache( $this->getAction() );
+                    $this->cacheDestroyer->clearTypo3Cache( $this->getAction() );
                     break;
                 default:
                     $this->cli_help();
                     break;
             } // END switch
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             $shellExitCode = 1;
-
         }
 
         return $shellExitCode;
     }
 
-    /**
-     * Clear caches
-     *
-     * @param string $cacheCmd
-     */
-    protected function clearTypo3Cache($cacheCmd) {
-        if($cacheCmd === 'all') {
-            $this->clearAllCache($cacheCmd);
-            return;
-        }
-        $this->clearCacheByCommand($cacheCmd);
-    }
 
-    /**
-     * @param $cacheCmd
-     */
-    protected function clearCacheByCommand($cacheCmd) {
-        GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager')->flushCachesInGroup($cacheCmd);
-        $this->callPostClearHooks($cacheCmd);
-    }
-
-    /**
-     * Clearing all the cache is a bit more nuclear than the other options
-     * @param $cacheCmd
-     */
-    protected function clearAllCache($cacheCmd) {
-        GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager')->flushCaches();
-        $this->callPostClearHooks($cacheCmd);
-        $this->forceDestroyReflectionCache();
-        if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cleartypo3cache']['forceRemoveTempCacheFiles']) {
-            static::quickDeleteDir('typo3temp/Cache');
-            if (GeneralUtility::compat_version('7.0')) {
-                static::quickDeleteDir('typo3temp/autoload');
-            }
-        }
-    }
-
-    /**
-     * Call any post-cache-clearing hooks from extensions, etc. This is lifted more or less verbatim from the
-     * DataHandler where it is normally called in the TYPO3 core;
-     *
-     * @param $cacheCmd
-     */
-    protected function callPostClearHooks($cacheCmd) {
-        $dataHandler = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\DataHandling\\DataHandler');
-        if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'])) {
-            $_params = array('cacheCmd' => strtolower($cacheCmd));
-            foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'] as $_funcRef) {
-                GeneralUtility::callUserFunction($_funcRef, $_params, $dataHandler);
-            }
-        }
-    }
-
-    /**
-     * @param $dir
-     * @return string
-     */
-    protected static function quickDeleteDir($dir) {
-        /**
-         * Only do this inside typo3temp
-         */
-        if (strpos($dir, 'typo3temp') !== 0) {
-            return;
-        }
-
-        $directory = PATH_site . $dir;
-        $offFolder = "$directory-off";
-
-        /**
-         * Make sure the backup folder doesn't exist
-         */
-        CommandUtility::exec('rm -rf ' . $offFolder);
-
-        /**
-         * Move the current folder away to effectively delete it instantly
-         */
-        CommandUtility::exec('mv ' . $directory . ' ' . $offFolder);
-
-        /**
-         * Remove the moved folder now that it's out of circulation
-         */
-        CommandUtility::exec('rm -rf ' . $offFolder);
-    }
-
-    /**
-     * TRUNCATE all tables that are named like 'cf_extbase%'.  This
-     * has occasionally been necessary in the past after running
-     * automated deployments.
-     */
-    protected function forceDestroyReflectionCache() {
-        $res = $GLOBALS['TYPO3_DB']->sql_query('SHOW TABLES LIKE "cf_extbase%"');
-        $truncate = array();
-        while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_row($res)) {
-            $truncate[] = $row[0];
-        }
-        if (count($truncate)) { foreach($truncate as $table) {
-            $GLOBALS['TYPO3_DB']->sql_query("TRUNCATE TABLE $table");
-        }}
-    }
 
     /**
      * @return string
@@ -170,5 +78,5 @@ class ClearCache extends \TYPO3\CMS\Core\Controller\CommandLineController {
 
 }
 
-$obj = GeneralUtility::makeInstance('CIC\\Cleartypo3cache\\ClearCache');
+$obj = GeneralUtility::makeInstance('CIC\Cleartypo3cache\ClearCache');
 exit($obj->cli_main($_SERVER['argv']));
---
+++ Classes/Service/CacheDestroyer.php
@@ -0,0 +1,109 @@
+<?php namespace CIC\Cleartypo3cache\Service;
+
+use TYPO3\CMS\Core\Utility\CommandUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class CacheDestroyer
+ * @package CIC\Cleartypo3cache\Service
+ */
+class CacheDestroyer {
+    /**
+     * Clear caches
+     *
+     * @param string $cacheCmd
+     */
+    public function clearTypo3Cache($cacheCmd) {
+        if($cacheCmd === 'all') {
+            $this->clearAllCache($cacheCmd);
+            return;
+        }
+        $this->clearCacheByCommand($cacheCmd);
+    }
+
+    /**
+     * @param $cacheCmd
+     */
+    protected function clearCacheByCommand($cacheCmd) {
+        GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager')->flushCachesInGroup($cacheCmd);
+        $this->callPostClearHooks($cacheCmd);
+    }
+
+    /**
+     * Clearing all the cache is a bit more nuclear than the other options
+     * @param $cacheCmd
+     */
+    public function clearAllCache($cacheCmd = 'all') {
+        GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager')->flushCaches();
+        $this->callPostClearHooks($cacheCmd);
+        $this->forceDestroyReflectionCache();
+        if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cleartypo3cache']['forceRemoveTempCacheFiles']) {
+            static::quickDeleteDir('typo3temp/Cache');
+            if (GeneralUtility::compat_version('7.0')) {
+                static::quickDeleteDir('typo3temp/autoload');
+            }
+        }
+    }
+
+    /**
+     * Call any post-cache-clearing hooks from extensions, etc. This is lifted more or less verbatim from the
+     * DataHandler where it is normally called in the TYPO3 core;
+     *
+     * @param $cacheCmd
+     */
+    protected function callPostClearHooks($cacheCmd) {
+        $dataHandler = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\DataHandling\\DataHandler');
+        if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'])) {
+            $_params = array('cacheCmd' => strtolower($cacheCmd));
+            foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'] as $_funcRef) {
+                GeneralUtility::callUserFunction($_funcRef, $_params, $dataHandler);
+            }
+        }
+    }
+
+    /**
+     * @param $dir
+     */
+    protected static function quickDeleteDir($dir) {
+        /**
+         * Only do this inside typo3temp
+         */
+        if (strpos($dir, 'typo3temp') !== 0) {
+            return;
+        }
+
+        $directory = PATH_site . $dir;
+        $offFolder = "$directory-off";
+
+        /**
+         * Make sure the backup folder doesn't exist
+         */
+        CommandUtility::exec('[ -d ' . $offFolder . ' ] && rm -rf ' . $offFolder);
+
+        /**
+         * Move the current folder away to effectively delete it instantly
+         */
+        CommandUtility::exec('[ -d ' . $directory . ' ] && mv ' . $directory . ' ' . $offFolder);
+
+        /**
+         * Remove the moved folder now that it's out of circulation
+         */
+        CommandUtility::exec('[ -d '.$offFolder.' ] && rm -rf ' . $offFolder);
+    }
+
+    /**
+     * TRUNCATE all tables that are named like 'cf_extbase%'.  This
+     * has occasionally been necessary in the past after running
+     * automated deployments.
+     */
+    protected function forceDestroyReflectionCache() {
+        $res = $GLOBALS['TYPO3_DB']->sql_query('SHOW TABLES LIKE "cf_extbase%"');
+        $truncate = array();
+        while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_row($res)) {
+            $truncate[] = $row[0];
+        }
+        if (count($truncate)) { foreach($truncate as $table) {
+            $GLOBALS['TYPO3_DB']->sql_query("TRUNCATE TABLE $table");
+        }}
+    }
+}
